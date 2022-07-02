43dbb1ba8da3ff053319484b2f73d636f8d2cdce
yiari <100484453+yiari@users.noreply.github.com>
2022-07-02 19:04:04 -0400

    cambios de yimar

---
+++ vistas/login.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Login - Sistema SIGAPYC</title>
+        <link href="css/bootstrap.min.css" rel="stylesheet" />
+        <link href="css/styles.css" rel="stylesheet" />
+        <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+    </head>
+    <body class="bg-opacity-75">
+        <div id="layoutAuthentication">
+            <div id="layoutAuthentication_content">
+                <main>
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-lg-5">
+                                <div class="card shadow-lg border-0 rounded-lg mt-5">
+                                    <div class="logo"><img src="img/logo.png" class="img-responsive" alt=""></div>
+                                    <div class="card-header"><h3 class="text-center font-weight-light my-4">Módulo de Alquileres</h3></div>
+                                    <div class="card-body">
+                                        <form method="POST" action="menuprincipal.php">
+                                            <div class="form-floating mb-3">
+                                                <input class="form-control" id="inputEmail" name= "usuario" type="text" placeholder="name@example.com" />
+                                                <label for="inputEmail">Ingrese su correo electrónico</label>
+                                            </div>
+                                            <div class="form-floating mb-3">
+                                                <input class="form-control" id="inputPassword" name= "contrasena" type="password" placeholder="Password" />
+                                                <label for="inputPassword">ingrese su contraseña</label>
+                                            </div>                                             
+                                            <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
+                                                <a class="small" href="password.html">¿Olvidó su contraseña?</a>
+                                                <button type= "submit" class="btn btn-primary">Login</button></a>
+                                            </div>                                         
+                                        </form>
+                                    </div>                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+            </div>    
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
